7513ed11b2817a82905da7ea0c487d87db95f13d
Emre <emrhayat1905@gmail.com>
2024-12-25 18:41:43 +0300

    initial commit

---
+++ app/src/main/java/com/emrehayat/artbook/ArtActivity.java
@@ -0,0 +1,40 @@
+package com.emrehayat.artbook;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.emrehayat.artbook.databinding.ActivityArtBinding;
+import com.emrehayat.artbook.databinding.ActivityMainBinding;
+
+public class ArtActivity extends AppCompatActivity {
+
+    private ActivityArtBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityArtBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void save (View view) {
+
+    }
+
+    public void selectImage (View view) {
+
+    }
+}
