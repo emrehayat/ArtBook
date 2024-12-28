9443dad32c1e29894f3c0538b294bf50213c51f9
Emre <emrhayat1905@gmail.com>
2024-12-28 15:20:41 +0300

    art book 4

--- app/src/main/java/com/emrehayat/artbook/MainActivity.java
+++ app/src/main/java/com/emrehayat/artbook/MainActivity.java
@@ -15,6 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.emrehayat.artbook.databinding.ActivityMainBinding;
 
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     ArrayList<Art> artArrayList;
+    ArtAdapter artAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         artArrayList = new ArrayList<>();
+
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        artAdapter = new ArtAdapter(artArrayList);
+        binding.recyclerView.setAdapter(artAdapter);
 
         getData();
     }
@@ -57,13 +63,11 @@ public class MainActivity extends AppCompatActivity {
                 Art art = new Art(name,id);
                 artArrayList.add(art);
             }
-
+            artAdapter.notifyDataSetChanged();
             cursor.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
     }
 
     @Override
@@ -78,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == R.id.add_art) {
             Intent intent = new Intent(this, ArtActivity.class);
+            intent.putExtra("info", "new");
             startActivity(intent);
         }
 
